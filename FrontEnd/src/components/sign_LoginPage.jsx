9e862e3dec8f393b9690905bd10b2d2dfafbdb17
Harsh <harsharyahi@gmail.com>
2026-06-24 20:43:30 +0530

    tailwind test 4

--- FrontEnd/src/components/sign_LoginPage.jsx
+++ FrontEnd/src/components/sign_LoginPage.jsx
@@ -18,7 +18,7 @@ function Sign_LoginPage(props) {
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="flex w-[900px] shadow-xl rounded-2xl overflow-hidden bg-white">
+      <div className="flex w-[500px] shadow-xl rounded-2xl overflow-hidden bg-white">
         {/* LEFT SIDE */}
         <div className="w-1/2 bg-blue-500 text-white flex flex-col justify-center items-center p-8">
           <img
