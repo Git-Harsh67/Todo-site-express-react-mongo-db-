d4d4bcf9988d5ad64487cd6cd14d4a7e551cc70d
Harsh <harsharyahi@gmail.com>
2026-09-24 14:37:11 +0530

    changed evn value

--- BackEnd/config/mongoDB.js
+++ BackEnd/config/mongoDB.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose")
 
 async function dataBase() {
     try {
-        await mongoose.connect(process.env.mongo_URL)
+        await mongoose.connect(process.env.MONGO_URL)
         console.log("db connected");
         
     } catch (error) {
